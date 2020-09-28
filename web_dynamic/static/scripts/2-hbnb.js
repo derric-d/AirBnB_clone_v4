6a3c0db14f9100d0d00eb5f8c2623c3f5a38f183
djso89 <danielso89@gmail.com>
2020-09-28 14:40:31 -0700

    task 3: fixed semistd

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,22 +1,24 @@
-$(function () {
-	const amendict = {};
+const $ = window.$;
 
-	$('div.amenities li input').change(
-		function () {
-			if ($(this).is(':checked')) {
-				amendict[($(this).attr('data-id'))] = $(this).attr('data-name');
-			} else {
-				delete amendict[($(this).attr('data-id'))]
-		}
-		if (Object.values(amendict).length > 0) {
-			$('.amenities h4').text(Object.values(amendict).join(', '));
-		} else {
-			$('.amenities h4').html('&nbsp');
-		}
-	});
+$(function () {
+  const amendict = {};
+
+  $('div.amenities li input').change(
+    function () {
+      if ($(this).is(':checked')) {
+        amendict[($(this).attr('data-id'))] = $(this).attr('data-name');
+      } else {
+        delete amendict[($(this).attr('data-id'))];
+      }
+      if (Object.values(amendict).length > 0) {
+        $('.amenities h4').text(Object.values(amendict).join(', '));
+      } else {
+        $('.amenities h4').html('&nbsp');
+      }
+    });
 });
 
-$.get('http://0.0.0.0:5001/api/v1/status/', function(data, stat) {
+$.get('http://0.0.0.0:5001/api/v1/status/', function (data, stat) {
   if (stat === 'success') {
     if (data.stat === 'OK') {
       $('#api_status').addClass('available');
