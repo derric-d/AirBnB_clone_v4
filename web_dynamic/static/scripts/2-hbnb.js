79c4ed27f0d76c44f62579a7255f74aa96338cf2
djso89 <danielso89@gmail.com>
2020-09-28 15:22:07 -0700

    data.stat to data.status

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,6 +1,6 @@
 const $ = window.$;
 
-$(function () {
+$(document).ready(function () {
   const amendict = {};
 
   $('div.amenities li input').change(
@@ -20,7 +20,7 @@ $(function () {
 
 $.get('http://0.0.0.0:5001/api/v1/status/', function (data, stat) {
   if (stat === 'success') {
-    if (data.stat === 'OK') {
+    if (data.status === 'OK') {
       $('#api_status').addClass('available');
     } else {
       $('#api_status').removeClass('available');
